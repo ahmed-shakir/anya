9262a4598517348aeef82c2dd4c9524c12e18aed
Ahmed Shakir <ahmed.shakir@supernovait.se>
2026-07-29 21:30:43 +0400

    add datastore config

--- shared/src/androidMain/kotlin/se/supernovait/anya/di/Modules.android.kt
+++ shared/src/androidMain/kotlin/se/supernovait/anya/di/Modules.android.kt
@@ -1,14 +1,21 @@
 package se.supernovait.anya.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.room.RoomDatabase
 import org.koin.dsl.module
 import se.supernovait.anya.app.data.database.DatabaseManager
 import se.supernovait.anya.app.data.local.CatDatabase
+import se.supernovait.anya.core.data.preferences.createDataStore
 import se.supernovait.anya.core.domain.util.applicationContext
 
 actual val platformModule = module {
 
     single<RoomDatabase.Builder<CatDatabase>> {
         DatabaseManager.createCatDatabase(context = applicationContext)
+    }
+
+    single<DataStore<Preferences>> {
+        createDataStore(context = applicationContext)
     }
 }
--- shared/src/iosMain/kotlin/se/supernovait/anya/di/Modules.ios.kt
+++ shared/src/iosMain/kotlin/se/supernovait/anya/di/Modules.ios.kt
@@ -1,13 +1,20 @@
 package se.supernovait.anya.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.room.RoomDatabase
 import org.koin.dsl.module
 import se.supernovait.anya.app.data.database.DatabaseManager
 import se.supernovait.anya.app.data.local.CatDatabase
+import se.supernovait.anya.core.data.preferences.createDataStore
 
 actual val platformModule = module {
 
     single<RoomDatabase.Builder<CatDatabase>> {
         DatabaseManager.createCatDatabase()
+    }
+
+    single<DataStore<Preferences>> {
+        createDataStore()
     }
 }
